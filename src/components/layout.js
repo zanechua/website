723614969e72eb57e3c0d75a367a1c009e0b39e3
Zane J. Chua <zane.jocelyn@gmail.com>
2020-06-15 17:09:02 +0000

    Add in 0U for server racks

--- src/components/layout.js
+++ src/components/layout.js
@@ -7,7 +7,7 @@ import Footer from './footer';
 function Layout(props) {
   const { children, className } = props;
   return (
-    <div className={`flex flex-col min-h-screen font-firacode text-gray-900 ${className || ''}`}>
+    <div className={`flex flex-col min-h-screen font-firacode ${className || ''}`}>
       <Header />
       <main className="flex flex-1 w-full max-w-4xl px-4 py-8 mx-auto md:px-8">
         {children}
