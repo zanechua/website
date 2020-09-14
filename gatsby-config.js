52635707d2f373892ad8189990a37fd1a06e2db1
Zane J. Chua <zane.jocelyn@gmail.com>
2020-09-14 09:38:27 +0000

    fix: google analytics not working

--- gatsby-config.js
+++ gatsby-config.js
@@ -16,21 +16,7 @@ module.exports = {
         // The property ID; the tracking code won't be generated without it
         trackingId: "UA-27711815-1",
         // Defines where to place the tracking script - `true` in the head and `false` in the body
-        head: true,
-        // Setting this parameter is optional
-        anonymize: true,
-        // Setting this parameter is also optional
-        respectDNT: true,
-        // Avoids sending pageview hits from custom paths
-        exclude: ["/preview/**", "/do-not-track/me/too/"],
-        // Delays sending pageview hits on route update (in milliseconds)
-        pageTransitionDelay: 0,
-        // Defers execution of google analytics script after page load
-        defer: true,
-        // Any additional optional fields
-        sampleRate: 5,
-        siteSpeedSampleRate: 10,
-        cookieDomain: "zanechua.com",
+        head: true
       },
     },
     `gatsby-plugin-eslint`,
