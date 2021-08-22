3f33c25c7af21eb71ac663f69bd1e6e64ca5e922
Zane J. Chua <zane@poowf.com>
2021-08-23 05:44:59 +0800

    refactor: redo eslint config and enforce conventional commits

---
+++ .prettierrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+  endOfLine: 'lf',
+  trailingComma: 'none',
+  arrowParens: 'avoid',
+  singleQuote: true,
+  jsxSingleQuote: false,
+  printWidth: 100,
+  semi: true,
+  jsxBracketSameLine: true,
+  jsxSingleQuote: false
+};
