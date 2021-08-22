6177b9f92eb40a138af079f3eb171c820a487da4
Zane J. Chua <zane@poowf.com>
2021-08-23 04:08:58 +0800

    feat: add service worker update button

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,7 +1,13 @@
+import useStore from './src/lib/zustand';
 import "./src/css/style.css";
 import "./src/css/markdown.css";
 import "./src/css/prism.css";
 import "./src/css/cat.css";
 require("prismjs/themes/prism-twilight.css");
-require("prismjs/plugins/line-numbers/prism-line-numbers.css")
-require("prismjs/plugins/command-line/prism-command-line.css")
+require("prismjs/plugins/line-numbers/prism-line-numbers.css");
+require("prismjs/plugins/command-line/prism-command-line.css");
+
+export const onServiceWorkerUpdateReady = () => {
+  useStore.setState({ hasSwUpdateReady: true })
+  // window.location.reload(true);
+};
