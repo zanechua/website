3c2d8268aa2141c5fe2e89c2f558a97def6f9e72
Zane J. Chua <zane@poowf.com>
2021-10-04 16:08:09 +0800

    style: fix broken toml code block

--- gatsby-config.js
+++ gatsby-config.js
@@ -121,17 +121,13 @@ module.exports = {
         printRejected: true, // Print removed selectors and processed file names
         develop: true, // Enable while using `gatsby develop`
         tailwind: true, // Enable tailwindcss support
-        // whitelist: ['whitelist'], // Don't remove this selector
         ignore: [
           'node_modules/prismjs/',
-          'node_modules/prism-themes/',
-          'src/css/style.css',
           'src/css/markdown.css',
           'src/css/prism.css',
           'src/css/prism-vsc-dark-plus.css',
           'src/css/prism-language-tabs.css'
         ] // Ignore files/folders
-        // purgeOnly : ['components/', '/main.css', 'bootstrap/'], // Purge only these files/folders
       }
     },
     {
