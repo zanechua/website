347ee937dedf48211250d08c4502781c9077ace2
Zane J. Chua <zane.jocelyn@gmail.com>
2022-03-12 14:57:57 +0800

    fix: paragraphs in comments

--- src/components/comment.js
+++ src/components/comment.js
@@ -2,11 +2,17 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 const Comment = ({ comment }) => {
+  console.log(comment);
+  const renderMessage = commentMessage => {
+    const messages = commentMessage.split('\n');
+    return messages.map(item => <p key={item}>{item}</p>);
+  };
+
   return (
     <div className="comment py-3">
       <h3 className="font-bold pb-2">{comment.name}</h3>
       <h4 className="font-bold pb-6">{comment.date}</h4>
-      <p className="pb-2">{comment.message}</p>
+      {renderMessage(comment.message)}
       <hr />
     </div>
   );
